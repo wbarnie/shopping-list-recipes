5d7deb47b21fd2bb53c4886e6305f918a3b1d895
wbarnie <webarnie18@gmail.com>
2020-07-21 09:38:57 -0400

    added security

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,16 +1,19 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {map, tap} from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { map, tap, take, exhaustMap } from 'rxjs/operators';
 
-import {Recipe} from '../recipes/recipe.model';
-import {RecipeService} from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
+import { AuthService } from '../auth/auth.service';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipeService: RecipeService) {
-  }
-
-  url = 'https://ng-course-recipe-book-e5daf.firebaseio.com/recipes.json';
+  url =    'https://ng-course-recipe-book-e5daf.firebaseio.com/recipes.json';
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
